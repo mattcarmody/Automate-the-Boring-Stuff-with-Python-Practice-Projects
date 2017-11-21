acf885bda0d5759036f9275043746d8c4fb6bb49
mattcarmody <33763384+mattcarmody@users.noreply.github.com>
2017-11-20 20:36:38 -0600

    Original Upload
    "Copy the previous grid value, and write code that uses it to print the image:
    ..OO.OO..
    .OOOOOOO.
    .OOOOOOO.
    ..OOOOO..
    ...OOO...
    ....O...."

---
+++ chap4PracProjCharGrid.py
@@ -0,0 +1,21 @@
+#!python3
+# chap4PracProjCharGrid.py - Transform list of lists.
+
+grid = [['.', '.', '.', '.', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['.', 'O', 'O', 'O', 'O', 'O'],
+        ['O', 'O', 'O', 'O', 'O', '.'],
+        ['O', 'O', 'O', 'O', '.', '.'],
+        ['.', 'O', 'O', '.', '.', '.'],
+        ['.', '.', '.', '.', '.', '.']]
+
+columns = len(grid[0])
+rows = len(grid)
+
+for j in range(columns):
+    temp = ''
+    for i in range(rows):
+        temp += grid[i][j]
+    print(temp)
