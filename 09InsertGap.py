22c2318371f0462b9154876e45f41243e628397a
Matt Carmody <carmodym2@gmail.com>
2018-02-03 16:14:45 -0600

    Remove unnecessary variable, rename variables & constants, add string formatters

--- 09InsertGap.py
+++ 09InsertGap.py
@@ -1,35 +1,28 @@
-#!/usr/bin/python3
-# chap9PracProjInsertGap.py
-
 import re
 import shutil
 import os
 
-insertionPoint = 3
+DIR = "{YOUR_DIRECTORY_HERE}"
+INSERTION_POINT = 3
 
 # Define regex to find numbered spam files.
-gapRegex = re.compile(r'''(
+gap_regex = re.compile(r'''(
     (spam)    # given prefix
     (\d+)      # number
     (.txt)    # given file type
     )''', re.VERBOSE)
     
-# Create list of the integers in the file names.
-numlst = []
-filelst = []
-for file in os.listdir('./chap9TestFiles'):
-	if gapRegex.search(file):
-		num = gapRegex.search(file).group(3)
-		filelst.append(file)
-		numlst.append(num)
-numlst = sorted(numlst)
-print(numlst)
-filelst = sorted(filelst)
-print(filelst)
+# Create list files matching regex criteria.
+files = []
+for _file in os.listdir(DIR):
+	if gap_regex.search(_file):
+		files.append(_file)
+files = sorted(files)
 
 # Rename files after insertion point, starting at the end, leaving a gap.
-for i in range(len(numlst)-1, insertionPoint-1, -1):
-	originalName = filelst[i]
-	newName = "spam" + str(i+1).zfill(3) + ".txt"
-	shutil.move('./chap9TestFiles/' + originalName, './chap9TestFiles/' + newName)
-	print("Renamed " + filelst[i])
+# This is to avoid overwriting.
+for i in range(len(files)-1, INSERTION_POINT-1, -1):
+	original_name = files[i]
+	new_name = "spam{}.txt".format(str(i+1).zfill(3))
+	shutil.move("{}{}".format(DIR, original_name), "{}/{}".format(DIR, new_name))
+	print("Renamed {}".format(files[i]))
