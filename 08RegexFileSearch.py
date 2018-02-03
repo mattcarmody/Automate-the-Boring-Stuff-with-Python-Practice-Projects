dd94cabfc55be799890f10ceb4ecde026a42f829
Matt Carmody <carmodym2@gmail.com>
2018-02-02 21:24:48 -0600

    Add conditional output, rename variables & constant, add string formatters

--- 08RegexFileSearch.py
+++ 08RegexFileSearch.py
@@ -1,20 +1,23 @@
-#! python3
-# chap8RegexFileSearch.py - Searches all .txt files in a location for matches.
+import os
+import re
 
-import re, os
+# End directory with a / (or \\ on Windows)
+DIRECTORY = '{YOUR_DIRECTORY_HERE}'
 
-# Define Regex.
-fileRegex = re.compile(str(input('What are you looking for?\n')))
+file_regex = re.compile(input('What are you looking for?\n'))
 
 # Search each file in the directory.
 results = []
-for file in os.listdir('C:\\Users\\Papa\\Documents\\mattPython\\automateTheBoringStuff'):
-    if file.endswith(".txt"):
-        fileHolder = open('C:\\Users\\Papa\\Documents\\mattPython\\automateTheBoringStuff\\' + file)
-        fileText = fileHolder.read()
-        if fileRegex.findall(fileText):
-            results.append(file)
-        fileHolder.close()
-print('The following files have a match:')
-for i in results:
-    print(i)
+for _file in os.listdir(DIRECTORY):
+    if _file.endswith(".txt"):
+        file_holder = open('{}{}'.format(DIRECTORY, _file))
+        file_text = file_holder.read()
+        if file_regex.findall(file_text):
+            results.append(_file)
+        file_holder.close()
+if results:
+    print('The following files have a match:')
+    for i in results:
+        print(i)
+else:
+    print("No match.")
