6f237c06a37fb6983c7938735b9d471b250f1af0
Matt Carmody <carmodym2@gmail.com>
2018-02-02 18:40:43 -0600

    Rename constant

--- 04CommaCode.py
+++ 04CommaCode.py
@@ -5,11 +5,11 @@ def colloquial(items):
     output += 'and {}'.format(items[-1])
     print(output)
     
-testInputs = [['apples', 'bananas', 'tofu', 'cats'],
-              ['rickshaws', 'go-karts', 'umbrellas', 'turtles', 
+TEST_INPUTS = [['apples', 'bananas', 'tofu', 'cats'],
+               ['rickshaws', 'go-karts', 'umbrellas', 'turtles', 
                 'banana peels'],
-              ['apples', 'oranges'],
-              ['lonely ranger']]
+               ['apples', 'oranges'],
+               ['lonely ranger']]
 
-for i in range(len(testInputs)):
-    colloquial(testInputs[i])
+for i in range(len(TEST_INPUTS)):
+    colloquial(TEST_INPUTS[i])
