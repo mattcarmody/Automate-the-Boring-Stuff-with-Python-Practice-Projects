df59a1e6d585100e2d96d324024ac1b7ddae6e97
Matt Carmody <carmodym2@gmail.com>
2018-02-05 20:16:05 -0600

    Rename a lot, add string formatters, other minor changes

--- 11ImageSiteDownloader.py
+++ 11ImageSiteDownloader.py
@@ -1,42 +1,37 @@
-#! /usr/bin/python3
-# chap11PracProjImageSiteDownloader.py
-
 import bs4
 import os
 import requests
 import sys
 
-searchTerm = sys.argv[1]
-baseUrl = 'https://imgur.com'
-searchUrl = baseUrl + '/search/score?q=' + searchTerm
+search_term = sys.argv[1] # "sankey" yields 4 results
+LOCAL_DEST = '/home/matt/AutomateBookLocal/imgur'
+BASE_URL = "https://imgur.com"
+url = "{}/search/score?q={}".format(BASE_URL, search_term)
 
-def getpage(destination):
-    res = requests.get(destination)
+def get_page(dest):
+    res = requests.get(dest)
     res.raise_for_status()
     soup = bs4.BeautifulSoup(res.text, "lxml")
     return res, soup
 
-res, soup = getpage(searchUrl)
+res, soup = get_page(url)
 
-# Loop through all links
 for link in soup.find_all('a'):
-    linkHere = link.get('href')
-    # Request page if link starts with '/gallery/' (is a search result)
-    if linkHere[0:9] == '/gallery/':
-        print(linkHere)
-        res2, soup2 = getpage(baseUrl + linkHere)
+    link = link.get('href')
+    # Request page if it's a search result
+    if link.startswith('/gallery/'):
+        img_res, img_soup = get_page("{}/{}".format(BASE_URL, link))
         # Find and download only post image(s)
-        divPostImages = soup2.find_all("div", {"class": "post-images"})
-        for tag in divPostImages:
-            imgTags = tag.find_all("img")
-            for img in imgTags:
+        post_images = img_soup.find_all("div", {"class": "post-images"})
+        for tag in post_images:
+            img_tags = tag.find_all("img")
+            for img in img_tags:
                 print(img.get('src'))
-                imgUrl = 'https:' + img.get('src')
-                imgRes = requests.get(imgUrl)
-                imgRes.raise_for_status()
-                
-                imgFile = open(os.path.join('imgur', os.path.basename(imgUrl)), 'wb')
-                for chunk in imgRes.iter_content(100000):
-                    imgFile.write(chunk)
-                imgFile.close()
+                img_url = 'https:{}'.format(img.get('src'))
+                img_res = requests.get(img_url)
+                img_res.raise_for_status()               
+                img_file = open(os.path.join(LOCAL_DEST, os.path.basename(img_url)), 'wb')
+                for chunk in img_res.iter_content(100000):
+                    img_file.write(chunk)
+                img_file.close()
                 print("Successful download!")
