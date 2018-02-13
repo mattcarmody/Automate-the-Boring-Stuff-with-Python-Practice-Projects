494eb9d5c2f630a9675472caf8176afe3199c46c
Matt Carmody <carmodym2@gmail.com>
2018-02-13 11:16:05 -0600

    Rename variable for clarity, trim header comments

--- 12SheetToTxt.py
+++ 12SheetToTxt.py
@@ -1,5 +1,4 @@
-#! /usr/bin/python3
-# chap12PracProjSheetToTxt.py - Copy contents of text files to a spreadsheet.
+# Copy contents of spreadsheet to a txt file.
 
 import openpyxl
 
@@ -9,8 +8,8 @@ wb = openpyxl.load_workbook('txtToSheet.xlsx')
 sheet = wb.active
 
 for j in range(1, sheet.max_column+1):
-	fileHere = open(files[j-1], 'w')
+	_file = open(files[j-1], 'w')
 	for i in range(1, sheet.max_row+1):
 		if sheet.cell(row=i, column=j).value:
-			fileHere.write(sheet.cell(row=i, column=j).value)
-	fileHere.close
+			_file.write(sheet.cell(row=i, column=j).value)
+	_file.close
