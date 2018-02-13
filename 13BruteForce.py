7c5a3220a8037ec7246cec7d181739cbea6408cb
Matt Carmody <carmodym2@gmail.com>
2018-02-13 11:50:20 -0600

    Restructure to address password bugs, rename variables and reword print messages for clarity

--- 13BruteForce.py
+++ 13BruteForce.py
@@ -1,20 +1,25 @@
-#! /usr/bin/python3
-# chap13PracProjBruteForce.py
-
 import PyPDF2
 
-dictionary = '/home/matt/AutomateBook/automate_online-materials/dictionary.txt'
-encryptedFile = '/home/matt/AutomateBook/pdfs/combinedminutes_encrypted.pdf'
-pdfReader = PyPDF2.PdfFileReader(open(encryptedFile, 'rb'))
+DICTIONARY = '/home/matt/AutomateBookLocal/automate_online-materials/dictionary.txt'
+ENCR_FILE = '/home/matt/AutomateBookLocal/pdfs/combinedminutes_encrypted.pdf'
+PWD_DEF = "12DEF34"
 
-dictFile = open(dictionary)
-dictList = dictFile.read().splitlines()
+pdf_reader = PyPDF2.PdfFileReader(open(ENCR_FILE, 'rb'))
+dict_content = open(DICTIONARY).read().splitlines()
 
-for word in dictList:
-	if pdfReader.decrypt(word):
-		print(word)
+print("Cracking your security...")
+pwd = PWD_DEF
+
+for word in dict_content:
+	if pdf_reader.decrypt(word):
+		pwd = word
 		break
-	if pdfReader.decrypt(word.lower()):
-		print(word.lower())
+	elif pdf_reader.decrypt(word.lower()):
+		pwd = word.lower()
 		break
+
+if pwd != PWD_DEF:
+    print("Your password is {}".format(pwd))
+else:
+    print("Either you have a half decent password, which isn't a dictionary word in all caps or lowercase, or the program botched the job")
 
