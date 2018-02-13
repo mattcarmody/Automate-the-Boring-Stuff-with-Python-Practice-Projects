b8d7bc25087d7ef14b7e67dc297afc6418af1611
Matt Carmody <carmodym2@gmail.com>
2018-02-13 11:08:03 -0600

    Address command line call bug, reword error message for clarity, remove unnecessary conditional, add string formatter

--- 12MultiplicationTable.py
+++ 12MultiplicationTable.py
@@ -5,28 +5,26 @@ import openpyxl, sys
 from openpyxl.styles import Font
 from openpyxl.utils import get_column_letter
 
-# Store N from command line call.
 if len(sys.argv) > 1:
     N = int(sys.argv[1])
+
+    # Create bold, frozen pane headers in row 1 & column 1.
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet.freeze_panes = 'B2'
+    bold = Font(bold=True)
+    for i in range(2, N+2):
+        sheet['A' + str(i)].font = bold
+        sheet['A' + str(i)].value = i-1
+        sheet[get_column_letter(i) + '1'].font = bold
+        sheet[get_column_letter(i) + '1'].value = i-1
+
+    # Populate multiplication table
+    for i in range(2, N+2):
+        for j in range(2, N+2):
+            sheet[get_column_letter(i) + str(j)] = (i-1)*(j-1)
+
+    wb.save('multTable{}.xlsx'.format(N))
+
 else:
-    print('Something went terribly wrong at the very beginning!')
-
-# Create bold, frozen pane headers in row 1 & column 1.
-wb = openpyxl.Workbook()
-sheet = wb.active
-sheet.freeze_panes = 'B2'
-bold = Font(bold=True)
-for i in range(2, N+2):
-    sheet['A' + str(i)].font = bold
-    sheet['A' + str(i)].value = i-1
-for i in range(2, N+2):
-    sheet[get_column_letter(i) + '1'].font = bold
-    sheet[get_column_letter(i) + '1'].value = i-1
-
-# Populate multiplication table
-for i in range(2, N+2):
-    for j in range(2, N+2):
-        sheet[get_column_letter(i) + str(j)] = (i-1)*(j-1)
-
-# Save file
-wb.save('multTable' + str(N) + '.xlsx')
+    print('Command line call must include the dimension of the square multiplication table (call N)')
