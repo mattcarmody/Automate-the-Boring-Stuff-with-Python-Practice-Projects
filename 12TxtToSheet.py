40eedb6c8d785ae7d782136ba92b67eff5c4f90c
Matt Carmody <carmodym2@gmail.com>
2018-02-13 11:25:27 -0600

    Remove unnecessry variables, rename variables for clarity, trim header comments

--- 12TxtToSheet.py
+++ 12TxtToSheet.py
@@ -1,19 +1,15 @@
-#! /usr/bin/python3
-# chap12PracProjTxtToSheet.py - Copy contents of text files to a spreadsheet.
+# Copy contents of text files to a spreadsheet.
 
 import openpyxl
 
-files = ['rows.txt', 'rowow.txt', 'rowowow.txt']
+files = [file1, file2, file3]
 
 wb = openpyxl.Workbook()
 sheet = wb.active
 
-count = 1
-for f in files:
-	fo = open(f)
-	lst = fo.readlines()
-	for i in range(len(lst)):
-		sheet.cell(row=i+1, column=count).value = lst[i]
-	count += 1
+for j, _file in enumerate(files):
+	content = open(_file).readlines()
+	for i in range(len(content)):
+		sheet.cell(row=i+1, column=j+1).value = content[i]
 
 wb.save('txtToSheet.xlsx')
