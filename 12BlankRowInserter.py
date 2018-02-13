6bb1f4aefc477ff8d540722f20f23ae71c876b2a
Matt Carmody <carmodym2@gmail.com>
2018-02-13 10:51:20 -0600

    Rename variables, resolve command line call bug, rename error message for clarity

--- 12BlankRowInserter.py
+++ 12BlankRowInserter.py
@@ -5,27 +5,26 @@ import sys, openpyxl
 from openpyxl.utils import get_column_letter
 
 # Store information passed from commmand line.
-if len(sys.argv) > 1:
+if len(sys.argv) >= 4:
     N = int(sys.argv[1])   # when to start skipping rows
     M = int(sys.argv[2])   # how many rows to skip
-    Z = sys.argv[3]        # file to read and play with
+    wb_name = sys.argv[3]        # file to read and play with
 else:
-    print('Something has gone horribly wrong!')
+    print('Command line call should pass the row number to begin skipping at, how many rows to skip and the file name. (call N M file)')
 
 # Read in passed spreadsheet.
-wb1 = openpyxl.load_workbook(Z)
-sheet1 = wb1.active
+wb_old = openpyxl.load_workbook(wb_name)
+sheet_old = wb_old.active
 
 # Write that in a new spreadsheet with a gap.
-wb2 = openpyxl.Workbook()
-sheet2 = wb2.active
+wb_new = openpyxl.Workbook()
+sheet_new = wb_new.active
 for j in range(1, N):
-    for i in range(1, sheet1.max_column+1):
-        sheet2[get_column_letter(i) + str(j)] = sheet1[get_column_letter(i) + str(j)].value
+    for i in range(1, sheet_old.max_column+1):
+        sheet_new[get_column_letter(i) + str(j)] = sheet_old[get_column_letter(i) + str(j)].value
 
-for j in range(N, sheet1.max_row+1):
-    for i in range(1, sheet1.max_column+1):
-        sheet2[get_column_letter(i) + str(j+M)] = sheet1[get_column_letter(i) + str(j)].value
+for j in range(N, sheet_old.max_row+1):
+    for i in range(1, sheet_old.max_column+1):
+        sheet_new[get_column_letter(i) + str(j+M)] = sheet_old[get_column_letter(i) + str(j)].value
 
-# Save file.
-wb2.save('blankRow.xlsx')
+wb_new.save('blankRow.xlsx')
