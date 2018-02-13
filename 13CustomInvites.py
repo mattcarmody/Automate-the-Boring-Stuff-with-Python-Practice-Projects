2510230f5657beb9b0d8b1cb3ef095868d502ac5
Matt Carmody <carmodym2@gmail.com>
2018-02-13 13:36:06 -0600

    Remove unnecessary variable, rename variables for clarity

--- 13CustomInvites.py
+++ 13CustomInvites.py
@@ -1,13 +1,9 @@
-#! /usr/bin/python3
-# chap13PracProjCustomInvites.py
-
 import docx
 
-guestFile = open('/home/matt/AutomateBook/automate_online-materials/guests.txt')
-guestList = guestFile.read().splitlines()
-doc = docx.Document('/home/matt/AutomateBook/invitations.docx')
+guest_list = open(FILE_HERE).read().splitlines()
+doc = docx.Document('/home/matt/AutomateBookLocal/invitations.docx')
 
-for guest in guestList:
+for guest in guest_list:
 	doc.add_paragraph('It would be a pleasure to have the company of', style='inviteCurly')
 	doc.add_paragraph(guest, style='inviteNorm')
 	doc.add_paragraph('at Memory Lane on the evening of', style='inviteCurly')
@@ -15,4 +11,4 @@ for guest in guestList:
 	doc.add_paragraph("at 7 o'clock", style="inviteCurly")
 	doc.add_page_break()
 	
-doc.save('/home/matt/AutomateBook/invitations2.docx')
+doc.save(FILE_HERE)
