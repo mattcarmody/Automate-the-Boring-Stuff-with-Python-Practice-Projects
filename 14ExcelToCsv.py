3925b61de94a0d42d7392f6743ff3b8ab21551f4
Matt Carmody <carmodym2@gmail.com>
2018-02-13 15:16:07 -0600

    Rename variables for clarity, add string formatters

--- 14ExcelToCsv.py
+++ 14ExcelToCsv.py
@@ -1,32 +1,23 @@
-#! python3
-# 14excelToCsv.py - Converts all Excel files in a directory to csv files.
-
 import csv, openpyxl, os
 from openpyxl.utils import get_column_letter
 
-# Loop through all files in directory.
-for file in os.listdir('C:\\Users\\Papa\\Documents\\mattPython\\'):
+DIR = "/home/matt/AutomateBookLocal/spreadsheets/"
+
+for _file in os.listdir(DIR):
     # Skip any non-Excel files.
-    if file.endswith('.xlsx'):
-        wb = openpyxl.load_workbook('C:\\Users\\Papa\\Documents\\mattPython\\' + file)
-        # Loop through all the sheets in each Excel file.
-        for sheet in wb.get_sheet_names():
-            # Create a CSV file for this sheet.
-            csvFile = open(file[:-5] + '_' + sheet + '.csv', 'w', newline='')
-            # Create the CSVwriter object for this file.
-            csvWriter = csv.writer(csvFile)
-            # Need a sheet object.
-            sheetObj = wb.get_sheet_by_name(sheet)
-            print('Loading rows & columns & cells for ' + file)
-            # Loop through each row.
-            for row in range(1, sheetObj.max_row+1):
-                rowData = []
-                # Loop through each cell in the row.
-                for column in range(1, sheetObj.max_column+1):
-                    # Append each cell in the row.
-                    rowData.append(sheetObj[get_column_letter(column) + str(row)].value)
-                # Append row to csvFile.
-                csvWriter.writerow(rowData)
-            # Save/close? CSV file.
-            csvFile.close()
+    if _file.endswith(".xlsx"):
+        wb = openpyxl.load_workbook("{}{}".format(DIR, _file))
+        # Create a CSV file for each sheet
+        for sheet_name in wb.get_sheet_names():
+            csv_file = open("{}_{}.csv".format(_file[:-5], sheet_name), 'w', newline='')
+            csv_writer = csv.writer(csv_file)
+            sheet = wb.get_sheet_by_name(sheet_name)
+            
+            for row in range(1, sheet.max_row+1):
+                row_data = []
+                for column in range(1, sheet.max_column+1):
+                    row_data.append(sheet["{}{}".format(get_column_letter(column), row)].value)
+                csv_writer.writerow(row_data)
+            csv_file.close()
+            print("Saving {}".format(csv_file.name))
             
