cde2d6d23b889b44d4772e581d6e08193b33ccfb
Matt Carmody <carmodym2@gmail.com>
2018-02-13 21:17:17 -0600

    Rename variables for clarity, add string formatters, remove personal info

--- 16ChoreAssignment.py
+++ 16ChoreAssignment.py
@@ -1,25 +1,24 @@
-#! /usr/bin/python3
-# chap16PracProjChoreAssignment.py
 # Randomly assigns a list of chores to people by email.
 # Requires equal number of chores and people.
 
 import random
 import smtplib
 
-destDict = {'carmodym2+1@gmail.com': [], 'carmodym2+2@gmail.com': [], 'carmodym2+3@gmail.com': [], 'carmodym2+4@gmail.com': []}
-choreList = ['dishes', 'bathroom', 'vacuum', 'walk dog', 'dust']
+MY_EMAIL = EMAIL_HERE
+DESTS = {EMAIL1: [], EMAIL2: [], EMAIL3: [], EMAIL4: []}
+CHORES = ['dishes', 'bathroom', 'vacuum', 'walk dog']
 
 # Assign each person a chore 
-for dest in destDict:
-    randomChore = random.choice(choreList)
-    choreList.remove(randomChore)
-    destDict[dest].append(randomChore)
+for dest in DESTS:
+    ran_chore = random.choice(CHORES)
+    CHORES.remove(ran_chore)
+    DESTS[dest].append(ran_chore)
 
 # Email each person their chore
-smtpObj = smtplib.SMTP('smtp.mail.yahoo.com', 587)
-smtpObj.ehlo()
-smtpObj.starttls()
-smtpObj.login(input('login'), input('password'))
-for dest in destDict:
-    smtpObj.sendmail("brownj4745@yahoo.com", dest, "Subject: Chore Assignment\n\nThis week's chore is: " + destDict[dest][0])   
-smtpObj.quit()
+smtp_obj = smtplib.SMTP('smtp.mail.yahoo.com', 587)
+smtp_obj.ehlo()
+smtp_obj.starttls()
+smtp_obj.login(input('login'), input('password'))
+for dest in DESTS:
+    smtp_obj.sendmail(MY_EMAIL, dest, "Subject: Chore Assignment\n\nThis week's chore is: {}".format(DESTS[dest][0]))   
+smtp_obj.quit()
