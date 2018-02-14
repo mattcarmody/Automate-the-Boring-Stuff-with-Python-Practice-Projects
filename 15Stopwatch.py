457551aa511aa8228636938ba2c1e60d9e41a0ca
Matt Carmody <carmodym2@gmail.com>
2018-02-13 21:05:57 -0600

    Rename variables and reform imports for clarity, add string formatters

--- 15Stopwatch.py
+++ 15Stopwatch.py
@@ -1,29 +1,30 @@
-#! python3
-# chap15PracProjStopwatch.py -  A simple stopwatch program with pretty output.
-#       Output is copied to the clipboard at the end.
+#  A simple stopwatch program with pretty output.
+#  Output is copied to the clipboard at the end.
 
-import time, pyperclip
+import pyperclip
+import time
 
 # Display the program's instructions.
 print('Press ENTER to begin. Then, press ENTER to "click" the stopwatch. Press Ctrl-C to quit.')
 input() # press Enter to begin.
 print('Started.')
-startTime = time.time() # get the first lap's start time
-lastTime = startTime
-lapNum = 1
-text = ''
+start_time = time.time() # get the first lap's start time
+last_click = start_time
+lap_num = 1
+cum_text = ''
 
 # Start tracking the lap times.
 try:
     while True:
-        input()
-        lapTime = round(time.time() - lastTime, 2)
-        totalTime = round(time.time() - startTime, 2)
-        print('Lap #%s: %s (%s)' % (str(lapNum).rjust(2), str(totalTime).rjust(5), str(lapTime).rjust(5)), end='')
-        text += ('Lap #' + str(lapNum).rjust(2) + ': ' + str(totalTime).rjust(5) +' (' + str(lapTime).rjust(5) + ')\n')
-        lapNum += 1
-        lastTime = time.time() # reset the last lap time
+        input() # press Enter to 'click' lap.
+        lap_time = round(time.time() - last_click, 2)
+        total_time = round(time.time() - start_time, 2)
+        new_text = 'Lap #{}: {} ({})'.format(str(lap_num).rjust(2), str(total_time).rjust(5), str(lap_time).rjust(5))
+        print(new_text, end='')
+        cum_text += '{}\n'.format(new_text)
+        lap_num += 1
+        last_click = time.time() # reset the last lap time
 except KeyboardInterrupt:
     # Handle the Ctrl-C exception to keep its error message from displaying.
-    pyperclip.copy(text)
+    pyperclip.copy(cum_text)
     print('\nDone. Text copied to clipboard.')
